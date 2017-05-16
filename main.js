ea51088b98011da19a8104e3c1614aca1809a81f
Andrea <didox1207@gmail.com>
2017-05-16 14:15:05 +0200

    scroll

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 var page = 0;
 
 window.onload = function() {
-  var div = document.createElement("div");
-  div.style.width = window.innerWidth;
-  div.style.height = window.innerHeight;
-  document.body.appendChild(div);
-}
-
-window.onscroll = function() {
-  console.log(document.body.scrollTop);
+  window.addEventListener('scroll', function() { console.log("scroll"); }, true);
 }
